f9f7cce8c34e9953d49218d29fcb9694f334a478
ebucis <lgarcia@ebucis.com>
2021-01-04 07:48:39 -0500

    adding example for @ES
    commenting crypto session dependent example (good for weekends)

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import time
 
 if __name__ == "__main__":
 	# parameters   symbol, chart_type, interval_type, unit, range_type, range_value, session = ""
-	dh = DataHost('BTCUSD', "BARS", "MINUTES", 1, "BARS", 1000, "Crypto")
+	#dh = DataHost('BTCUSD', "BARS", "MINUTES", 1, "BARS", 1000, "Crypto")
+	dh = DataHost('@ES', "BARS", "MINUTES", 1, "DAYS", 1, "")
 
 	#we wait for history to be deployed...
 	# TODO: create a more elegant way	
